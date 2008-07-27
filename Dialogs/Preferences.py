aa81b1bfc0e87b87d42008f52a6a513254285b35
Francesco Piccinno <stack.box@gmail.com>
2008-07-27 18:31:29 +0000

    Copyright headers and fix in Preference
    git-svn-id: http://svn.umitproject.org/svnroot/umit/branch/PacketManipulator@3230 1105ee14-b0fa-0310-85a5-ff3eed429ff7

--- Dialogs/Preferences.py
+++ Dialogs/Preferences.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2008 Adriano Monteiro Marques
+#
+# Author: Francesco Piccinno <stack.box@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
 import gtk
 
 class Page(gtk.VBox):
@@ -23,12 +43,17 @@ class PreferenceDialog(gtk.Dialog):
              gtk.STOCK_APPLY, gtk.RESPONSE_ACCEPT)
         )
 
-        self.store = gtk.ListStore(str)
+        self.store = gtk.ListStore(str, str)
         self.tree = gtk.TreeView(self.store)
 
         self.tree.append_column(
-            gtk.TreeViewColumn('', gtk.CellRendererText(), text=0))
+            gtk.TreeViewColumn('', gtk.CellRendererPixbuf(), stock_id=0))
+
+        self.tree.append_column(
+            gtk.TreeViewColumn('', gtk.CellRendererText(), text=1))
+
         self.tree.set_headers_visible(False)
+        self.tree.set_rules_hint(True)
 
         sw = gtk.ScrolledWindow()
         sw.set_policy(gtk.POLICY_NEVER, gtk.POLICY_AUTOMATIC)
@@ -51,7 +76,7 @@ class PreferenceDialog(gtk.Dialog):
         self.vbox.set_spacing(4)
 
     def __populate(self):
-        self.store.append(["GUI"])
+        self.store.append([gtk.STOCK_PREFERENCES, "GUI Preferences"])
         self.notebook.append_page(GUIPage())
 
 if __name__ == "__main__":
