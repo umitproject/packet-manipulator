f1d3da88865a587a3b71675c3ec5a286c4e56d00
Francesco Piccinno <stack.box@gmail.com>
2008-08-16 16:06:12 +0000

    Locales
    git-svn-id: http://svn.umitproject.org/svnroot/umit/branch/PacketManipulator@3484 1105ee14-b0fa-0310-85a5-ff3eed429ff7

--- scripts/i18n/update.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python pygettext.py PM/ PM/Backend/ PM/Backend/Abstract PM/Backend/Abstract/BaseContext PM/Backend/Abstract/Context PM/Backend/Scapy PM/Backend/Scapy/Context PM/Manager PM/Core PM/Gui PM/Gui/Core PM/Gui/Tab PM/Gui/Pages PM/Gui/Dialogs PM/Gui/Widgets PM/higwidgets
