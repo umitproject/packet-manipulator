6ab78ea25dba4aadd43a937785b6a07d730a3bc1
Francesco Piccinno <stack.box@gmail.com>
2008-11-03 16:14:08 +0000

    Traceroute plugin
    git-svn-id: http://svn.umitproject.org/svnroot/umit/branch/PacketManipulator@3778 1105ee14-b0fa-0310-85a5-ff3eed429ff7

---
+++ plugins/traceroute/sources/libtrace/tracert.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2008 Adriano Monteiro Marques
+#
+# Author: Francesco Piccinno <stack.box@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+import urllib
+from PM.Core.Logger import log
+
+def get_my_ip():
+    url = urllib.URLopener()
+    resp = url.open('http://myip.dk')
+    html = resp.read(256)
+    
+    end = html.find("</title>")
+    start = html.find("address is: ") + 12
+
+    return html[start:end].strip()
+
+def generate_map(points):
+    i = len(points) - 1
+    last = None
+
+    while i >= 0:
+        if points[i][1] is not None:
+            last = points[i][1]
+        else:
+            points[i] = (points[i][0], last)
+
+        i -= 1
+
+    i = 0
+    last = None
+
+    while i < len(points):
+        if points[i][1] is not None:
+            last = points[i][1]
+        else:
+            points[i][1] = (points[i][0], last)
+
+        i += 1
+
+    html = []
+
+    for point in points:
+        html.append("new GLatLng(%f, %f)" % (point[1][1], point[1][0]))
+
+    head = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+<title>PacketManipulator: Traceroute</title>
+<script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=ABQIAAAAQQRAsOk3uqvy3Hwwo4CclBTrVPfEE8Ms0qPwyRfPn-DOTlpaLBTvTHRCdf2V6KbzW7PZFYLT8wFD0A"
+        type="text/javascript"></script>
+<style type="text/css">
+v\:* {behavior:url(#default#VML);}
+html, body {width: 100%; height: 100%}
+body {margin-top: 0px; margin-right: 0px; margin-left: 0px; margin-bottom: 0px}
+</style>
+<script type="text/javascript">
+function initialize() {
+  if (GBrowserIsCompatible()) {
+    var map = new GMap2(document.getElementById("map_canvas"));
+    map.setCenter(new GLatLng("""
+    
+    head += "%s,%s%s" % (points[0][1][1], points[0][1][0], "), 0);")
+    head += "var polyline = new GPolyline(["
+    head += ",".join(html)
+    head += """], "#0000ff", 5);
+		map.addOverlay(polyline);"""
+
+    head += """
+        map.addControl(new GSmallMapControl());
+        map.addControl(new GMapTypeControl());
+
+        function createMarker(point, info_text) {
+          var marker = new GMarker(point, {});
+
+          GEvent.addListener(marker, "click", function() {
+            marker.openInfoWindowHtml(info_text);
+          });
+
+          return marker;
+        }
+    """
+
+    for point in html:
+        head += "map.addOverlay(createMarker(%s, \"%s\"));" % (point, points.pop(0)[0])
+
+    head += """
+        if (window.attachEvent) { 
+          window.attachEvent("onresize", function() {this.map.onResize()} ); 
+        } else { 
+          window.addEventListener("resize", function() {this.map.onResize()} , false);
+        } 
+    }
+}  
+</script>
+</head>
+
+<body onload="initialize()" onunload="GUnload()">
+<div id="map_canvas" style="width: 100%; height: 100%;"></div>
+<div id="message">Traceroute for PacketManipulator</div>
+</body>
+</html>"""
+
+    return head 
+
+def create_map(ans, locator):
+    log.debug("Creating map")
+
+    dct = ans.get_trace()
+
+    key = dct.keys()[0]
+    routes = dct[key].items()
+    routes.sort()
+
+    ip = get_my_ip()
+    last = locator.lon_lat(ip)
+
+    points = []
+
+    for k, (ip, is_ok) in routes:
+        loc = locator.lon_lat(ip)
+
+        if loc is None:
+            loc = last
+        else:
+            last = loc
+
+        points.append((ip, loc))
+
+    return generate_map(points)
---
+++ plugins/traceroute/sources/main.py
@@ -0,0 +1,284 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2008 Adriano Monteiro Marques
+#
+# Author: Francesco Piccinno <stack.box@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+import sys, os, os.path
+
+import gtk
+import gobject
+
+import GeoIP
+
+from libtrace import tracert
+
+from threading import Thread
+
+from PM.Backend import StaticContext, traceroute
+from PM.Core.Logger import log
+from PM.Core.Atoms import generate_traceback
+
+from PM.Gui.Core.App import PMApp
+from PM.Gui.Plugins.Engine import Plugin
+from PM.Gui.Sessions.Base import Session
+
+from PM.Gui.Sessions import SessionType
+from PM.Gui.Pages.Base import Perspective
+
+from PM.Core.Errors import PMErrorException
+from PM.Manager.PreferenceManager import Prefs
+
+try:
+    import webkit
+except ImportError:
+    raise PMErrorException("I need python binding for webkit")
+
+if Prefs()['backend.system'].value.lower() != 'scapy':
+    raise PMErrorException("I need scapy to work!")
+
+_ = str
+glocator = None
+
+class TracerouteMap(Perspective):
+    icon = gtk.STOCK_INFO
+    title = _('Visual traceroute')
+
+class Traceroute(Perspective):
+    icon = gtk.STOCK_INFO
+    title = _('Traceroute')
+
+    def create_ui(self):
+        self.toolbar = gtk.Toolbar()
+        
+        # Entry / dport / maxttl / timeout
+        self.target = gtk.Entry()
+        self.dport = gtk.SpinButton(gtk.Adjustment(80, 1, 65535, 1, 1))
+        self.maxttl = gtk.SpinButton(gtk.Adjustment(30, 1, 255, 1, 1))
+        self.timeout = gtk.SpinButton(gtk.Adjustment(2, 1, 10, 1, 1))
+
+        for label, widget in zip((_("Target:"), _("Port:"),
+                                  _("Max. TTL:"), _("Timeout:")),
+                                 (self.target, self.dport,
+                                  self.maxttl, self.timeout)):
+
+            item = gtk.ToolItem()
+
+            lbl = gtk.Label(label)
+            lbl.set_alignment(0, 0.5)
+
+            hbox = gtk.HBox(False, 2)
+            hbox.set_border_width(2)
+
+            hbox.pack_start(lbl, False, False)
+            hbox.pack_start(widget)
+
+            item.add(hbox)
+
+            if widget is self.target:
+                item.set_expand(True)
+
+            self.toolbar.insert(item, -1)
+
+        act = gtk.Action(None, None, _('Start tracing'), gtk.STOCK_CONNECT)
+        act.connect('activate', self.__on_trace)
+
+        self.toolbar.insert(act.create_tool_item(), -1)
+
+        self.pack_start(self.toolbar, False, False)
+
+        sw = gtk.ScrolledWindow()
+        sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        sw.set_shadow_type(gtk.SHADOW_ETCHED_IN)
+
+        # Protocol / Information
+        self.store = gtk.ListStore(int, str, str)
+        self.tree = gtk.TreeView(self.store)
+
+        self.tree.append_column(gtk.TreeViewColumn('TTL', gtk.CellRendererText(), text=0))
+        self.tree.append_column(gtk.TreeViewColumn('IP', gtk.CellRendererText(), text=1))
+        self.tree.append_column(gtk.TreeViewColumn('Time', gtk.CellRendererText(), text=2))
+
+        sw.add(self.tree)
+
+        self.pack_start(sw)
+        self.show_all()
+
+    def __on_trace(self, action):
+        self.toolbar.set_sensitive(False)
+        self.store.clear()
+
+        self.store.append([0, _("Tracing..."), ""])
+
+        thread = Thread(target=self.__do_trace)
+        thread.setDaemon(True)
+
+        thread.start()
+
+    def __do_trace(self):
+        try:
+            dport = self.dport.get_value_as_int()
+            maxttl = self.maxttl.get_value_as_int()
+            timeout = self.timeout.get_value_as_int()
+            target = self.target.get_text()
+
+            log.debug("Starting %s (%s, dport=%d, maxttl=%d, timeout=%d)" %
+                      (traceroute, target, dport, maxttl, timeout))
+
+            ans, unans = traceroute(target, dport, maxttl=maxttl,
+                                    timeout=timeout, verbose=True)
+
+            self.session.context.set_trace(ans, unans)
+            gobject.idle_add(self.session.reload)
+
+        except Exception, err:
+            self.session.context.set_trace(None, err)
+            gobject.idle_add(self.session.reload)
+
+            log.error("Exception in tracert:")
+            log.error(generate_traceback())
+        finally:
+            self.toolbar.set_sensitive(True)
+
+    def populate(self):
+        ret = self.session.context.data
+
+        if ret is None or len(ret) != 2 or not isinstance(ret, list):
+            return
+        
+        if ret[0] is None and isinstance(ret[1], Exception):
+            self.store.clear()
+            self.store.append([0, str(ret[1]), ""])
+            return
+
+        if not hasattr(ret[0], '__iter__') or \
+           not hasattr(ret[1], '__iter__'):
+            log.debug("Not a valid trace")
+            return
+
+        self.store.clear()
+
+        for i in ret[0]:
+            self.store.append([i[0].ttl, i[1].src, "%.3f" % (i[1].time - i[0].time)])
+
+class TracerouteMap(Perspective):
+    icon = gtk.STOCK_INFO
+    title = _('Visual traceroute')
+
+    def create_ui(self):
+        self.webview = webkit.WebView()
+        
+        sw = gtk.ScrolledWindow()
+        sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        sw.set_shadow_type(gtk.SHADOW_ETCHED_IN)
+        sw.add(self.webview)
+
+        self.pack_start(sw)
+
+        self.show_all()
+
+    def create_map(self):
+        log.debug("Trying to create a valid map")
+
+        ret = self.session.context.data
+
+        if ret is None or len(ret) != 2 or \
+           not isinstance(ret, list) or \
+           not hasattr(ret[0], '__iter__') or \
+           not hasattr(ret[1], '__iter__'):
+
+            self.webview.load_string("", "text/html", "iso-8859-15", "about:blank")
+            return
+
+        try:
+            global glocator
+
+            map = tracert.create_map(ret[0], glocator)
+            self.webview.load_string(map, "text/html", "iso-8859-15", "file:///")
+
+            log.info("Plotted")
+        except Exception, err:
+            log.error("Error while plotting")
+            log.error(generate_traceback())
+
+            self.webview.load_string("", "text/html", "iso-8859-15", "about:blank")
+
+class TracerouteContext(StaticContext):
+    def __init__(self, fname=None):
+        StaticContext.__init__(self, 'Traceroute', fname)
+        self.status = self.SAVED
+
+    def set_trace(self, ans, unans):
+        self.set_data([ans, unans])
+
+class TracerouteSession(Session):
+    session_name = "TRACEROUTE"
+    session_menu = "Traceroute"
+    session_orientation = gtk.ORIENTATION_HORIZONTAL
+
+    def create_ui(self):
+        self.trace_page = self.add_perspective(Traceroute, True,
+                                               True, False)
+        self.map_page = self.add_perspective(TracerouteMap, False,
+                                             True, False)
+
+        self.reload()
+        self.pack_start(self.paned)
+        self.show_all()
+
+    def reload_editor(self):
+        self.map_page.create_map()
+    
+    def reload_container(self, packet=None):
+        self.trace_page.populate()
+
+class Locator(object):
+    def __init__(self, path):
+        self.city = GeoIP.open(
+            os.path.join(path, 'GeoLiteCity.dat'),
+            GeoIP.GEOIP_STANDARD
+        )
+
+    def lon_lat(self, ip):
+        dct = self.city.record_by_addr(ip)
+
+        try:
+            return (dct['longitude'], dct['latitude'])
+        except:
+            return None
+
+class TraceroutePlugin(Plugin):
+    def start(self, reader):
+        if reader:
+            catalog = reader.bind_translation("traceroute")
+
+            if catalog:
+                global _
+                _ = catalog.gettext
+
+            ret = reader.extract_file('data/GeoLiteCity.dat')
+
+            global glocator
+            glocator = Locator(os.path.dirname(ret))
+
+            log.debug(glocator)
+
+        id = PMApp().main_window.register_session(TracerouteSession,
+                                                  TracerouteContext)
+        log.debug("Traceroute session binded with id %d" % id)
+
+__plugins__ = [TraceroutePlugin]
