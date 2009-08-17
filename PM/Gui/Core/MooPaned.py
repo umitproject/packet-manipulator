2f8cb3c674adbf1827200f8d83ed18ff96b644e3
Francesco Piccinno <stack.box@gmail.com>
2009-08-17 18:55:01 +0000

    Moo paned fixes
    git-svn-id: http://svn.umitproject.org/svnroot/umit/branch/PacketManipulator@5283 1105ee14-b0fa-0310-85a5-ff3eed429ff7

--- PM/Gui/Core/MooPaned.py
+++ PM/Gui/Core/MooPaned.py
@@ -1,21 +1,21 @@
-#!/usr/bin/env python            
-# -*- coding: utf-8 -*-          
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Copyright (C) 2008 Adriano Monteiro Marques
-#                                            
+#
 # Author: Francesco Piccinno <stack.box@gmail.com>
-#                                                 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or   
-# (at your option) any later version.                                 
-#                                                                     
-# This program is distributed in the hope that it will be useful,     
-# but WITHOUT ANY WARRANTY; without even the implied warranty of      
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
-# GNU General Public License for more details.                        
-#                                                                     
-# You should have received a copy of the GNU General Public License   
-# along with this program; if not, write to the Free Software         
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 """
@@ -50,7 +50,7 @@ class UmitPaned(BigPaned):
 
     def get_all_pos(self):
         return [gtk.POS_TOP, gtk.POS_BOTTOM, gtk.POS_LEFT, gtk.POS_RIGHT]
-        
+
     def get_all_paneds(self):
         for pos in self.get_all_pos():
             yield self.get_paned(pos)
