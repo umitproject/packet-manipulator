73b2b4cb981fe378f4a3fb4a9065dcf5f7895f47
Shu Yang Quek <quekshuy@gmail.com>
2010-07-07 17:43:25 +0000

    Modified scripts/cppm -- now can build and run PM with dev version of BTSniffer in PM_copy. (Done for Ubuntu LTS 10.04)
    git-svn-id: http://svn.umitproject.org/svnroot/umit/packet-manipulator/branches/BTSniff@5717 1105ee14-b0fa-0310-85a5-ff3eed429ff7

--- PM_copy/umit/pm/backend/bt_sniffer/crack.py
+++ PM_copy/umit/pm/backend/bt_sniffer/crack.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2008, 2009 Adriano Monteiro Marques
+#
+# Author: Quek Shu Yang <quekshuy@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
 
 import subprocess, os, re
 import sys, threading, string
@@ -15,8 +35,6 @@ LMP_OP1_COMB = 9
 LMP_OP1_AU_RAND = 11
 LMP_OP1_SRES = 12
 LMP_PINCRACK_OPCODES = [LMP_OP1_IN_RAND, LMP_OP1_COMB, LMP_OP1_AU_RAND, LMP_OP1_SRES]
-
-
 
 class PinCrackData(object):
     """
@@ -253,8 +271,6 @@ class _pincrackrunner(object):
 #######################################
 #     Internal methods                #
 #######################################
-
-__originates_master = True
 
 def _create_pincrackdata(state, is_pin_master):
     log.debug('1')
