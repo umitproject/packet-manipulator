014591b15264ae1a765065fb0fc419f09789742b
Guilherme Rezende <guilhermebr@gmail.com>
2011-05-30 00:49:44 -0300

    Finished sip-monitor skeleton

--- audits/passive/sip/sources/main.py
+++ audits/passive/sip/sources/main.py
@@ -31,63 +31,120 @@ from umit.pm.manager.sessionmanager import SessionManager
 SIP_NAME = 'dissector.sip'
 SIP_PORTS = (5060, 5061)
 
-def sip_dissector():
+SIP_REQUEST  = 0
+SIP_RESPONSE = 1
 
-    manager = AuditManager()
-    sessions = SessionManager()
-    
-    def sip(mpkt):
-        
-        sess = sessions.is_first_pkt_from_server(mpkt, SIP_PORTS, SIP_NAME)
+sip_fields = None
 
-        if sess and not sess.data:
-            payload = mpkt.data
+class SipSession(object):
+     
+     def __init__(self, mpkt, manager, sessions, sip_type):
+          self.manager = manager
+          self.payload = mpkt.data
+           
+          self.sess = sessions.lookup_session(mpkt, SIP_PORTS, SIP_NAME)
+          
+          if not self.sess:
+               self.sess = sessions.lookup_session(mpkt, SIP_PORTS, SIP_NAME, True)
+               self.sess.data = dict(
+                    map(lambda x: (x, 'None'), sip_fields.split(','))
+               )
+               
+          print self.sess.data
+          
+          if sip_type == SIP_REQUEST:
+               return self.sip_request(mpkt)
+          else: 
+               return self.sip_response(mpkt)
+            
+     def sip_request(self, mpkt):
+                        
+          if self.payload.startswith('REGISTER'):
+               print 'REGISTER'
+                              
+          elif self.payload.startswith('INVITE'):
+               print 'INVITE'
+               
+          end = self.payload.find('\n')
+          while end is not -1:
+               print "DATA: %s" % self.payload[:end]
+               self.payload = self.payload[end + 1:]
+               end = self.payload.find('\n')
 
-            if payload and payload.startswith('REGISTER'):
-                banner = payload[8:].strip()
-                manager.user_msg('SIP : %s:%d REGISTER: %s' % \
-                                 (mpkt.l3_src, mpkt.l4_src, banner),
-                                 6, SIP_NAME)
-            sessions.delete_session(sess)
-            return
-        
-         # Skip empty and server packets
-        if mpkt.l4_dst not in SIP_PORTS or not mpkt.data:
-            return
 
-        payload = mpkt.data.strip()
-        manager.user_msg('SIP : %s:%d -> %s' % \
-                        (mpkt.l3_dst, mpkt.l4_dst, payload),
-                        6, SIP_NAME)
-                             
-        sessions.delete_session(sess)
+               
+          self.manager.user_msg('SIP REQUEST: %s:%d' % \
+                           (mpkt.l3_src, mpkt.l4_src), 6, SIP_NAME)
 
-        return sip
+     def sip_response(self, mpkt):
+       
+          self.manager.user_msg('SIP RESPONSE: %s:%d'  % \
+                           (mpkt.l3_src, mpkt.l4_src),
+                           6, SIP_NAME)
+   
     
 class SIPMonitor(Plugin, PassiveAudit):
-    def start(self, reader):
-        self.manager = AuditManager()
-        self.dissector = sip_dissector()
+     def start(self, reader):
+          self.manager = AuditManager()
+          self.sessions = SessionManager()
+       
+          conf = self.manager.get_configuration(SIP_NAME)
+          
+          global sip_fields
+                         
+          sip_fields = conf['sip_fields']
+          
 
-    def register_decoders(self):
+     def register_decoders(self):
 
-        self.manager.register_hook_point('sip')
+          self.manager.register_hook_point('sip')
 
-        for port in SIP_PORTS:
-            self.manager.add_dissector(APP_LAYER_UDP, port,
-                                           self.dissector)
+          for port in SIP_PORTS:
+               self.manager.add_dissector(APP_LAYER_UDP, port,
+                                          self.__sip_dissector)
         
 
-    def stop(self):
-        for port in SIP_PORTS:
-            self.manager.remove_dissector(APP_LAYER_UDP, port,
-                                           self.dissector)
+     def stop(self):
+          for port in SIP_PORTS:
+               self.manager.remove_dissector(APP_LAYER_UDP, port,
+                                             self.__sip_dissector)
+               
+               self.manager.deregister_hook_point('sip')
+               
+     def __sip_dissector(self, mpkt):
+          
+          payload = mpkt.data
 
+          
+          if not payload:
+               return None
+          
+          #print payload
+
+          if payload.startswith('SIP/'):
+               sip_type = SIP_RESPONSE
+          elif payload.find('SIP/'):
+               sip_type = SIP_REQUEST
+          else:
+               return None
+
+               
+          obj = SipSession(mpkt, self.manager, self.sessions, sip_type)
+
+          
+               
+               
 __plugins__ = [SIPMonitor]
 __plugins_deps__ = [('SIPDissector', ['UDPDecoder'], ['SIPDissector-1.0'], []),]
 __author__ = ['Guilherme Rezende']
 __audit_type__ = 0
 __protocols__ = (('udp', 5060), ('udp', 5061), ('sip', None))
+__configurations__ = ((SIP_NAME, {
+    'sip_fields' : ["contact,to,via,from,user-agent,server,authorization,www-authenticate",
+
+                    'A coma separated string of sip fields'],
+    }),
+)
 __vulnerabilities__ = (('SIP dissector', {
     'description' : 'SIP Monitor plugin'
         'The Session Initiation Protocol (SIP) is an IETF-defined signaling protocol,'
